41c8ee082b4c1e035e7eb32daec81fc48bea6283
Lourdes Pérez <lourdesperez040402@gmail.com>
2024-11-16 16:59:04 -0600

    11.16.24_4.56_pm_templates
    se creó la template index y la base.html

--- sdp/sdp/urls.py
+++ sdp/sdp/urls.py
@@ -18,10 +18,8 @@ from django.contrib import admin
 from django.urls import path, include
 
 from django.contrib.auth.views import LogoutView
-from proyecto.views import index
 from django.conf import settings
 from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('index/',index,name='index' )
 ]
